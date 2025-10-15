6fcb8e16ff2b963edfddc42038144c7799ebff70
general electrix <general.electrix@gmail.com>
2025-10-14 18:26:33 -0700

    Move GenericStrobe out of the way.

--- src/fixture/profile/lumasphere.rs
+++ src/fixture/profile/lumasphere.rs
@@ -262,3 +262,43 @@ where
         wrap(Intensity(v))
     });
 }
+
+/// Most basic strobe control - active/not, plus rate.
+#[derive(Default, Clone, Debug)]
+pub struct GenericStrobe {
+    pub on: bool,
+    pub rate: UnipolarFloat,
+}
+
+impl GenericStrobe {
+    pub fn on(&self) -> bool {
+        self.on
+    }
+
+    pub fn rate(&self) -> UnipolarFloat {
+        self.rate
+    }
+
+    pub fn emit_state<F>(&self, emit: &mut F)
+    where
+        F: FnMut(GenericStrobeStateChange),
+    {
+        use GenericStrobeStateChange::*;
+        emit(On(self.on));
+        emit(Rate(self.rate));
+    }
+
+    pub fn handle_state_change(&mut self, sc: &GenericStrobeStateChange) {
+        use GenericStrobeStateChange::*;
+        match sc {
+            On(v) => self.on = *v,
+            Rate(v) => self.rate = *v,
+        }
+    }
+}
+
+#[derive(Clone, Copy, Debug)]
+pub enum GenericStrobeStateChange {
+    On(bool),
+    Rate(UnipolarFloat),
+}
